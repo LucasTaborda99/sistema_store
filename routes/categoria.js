b8a89633f41ed080f2d7e6c32e2f870273b0d0ad
lucas.taborda <lucas.taborda@fertipar.com.br>
2023-03-28 09:38:02 -0300

    Added route '/delete' and edited route '/get' and 'update'

--- routes/categoria.js
+++ routes/categoria.js
@@ -19,10 +19,14 @@ router.post('/adicionarCategoria', aut.autenticacaoToken, verRole.verificaRole, 
 router.get('/get', aut.autenticacaoToken, (req, res, next) => {
     let query = "SELECT * FROM categoria ORDER BY id"
     connection.query(query, (err, results) => {
-        if(!err){
-            return res.status(200).json(results)
-        } else {
+        if(err){
             return res.status(500).json(err)
+        } else {
+            if(results.length <= 0) {
+                return res.status(404).json({message: 'Nenhuma categoria encontrada'})
+            } else {
+                return res.status(200).json(results)
+            }
         }
     })
 })
@@ -33,11 +37,27 @@ router.patch('/update', aut.autenticacaoToken, verRole.verificaRole, (req, res, 
     connection.query(query, [produto.nome, produto.id], (err, results) => {
         if(!err) {
             if(results.affectedRows == 0) {
-                return res.status(404).json({message: "ID da categoria não encontrado"})
+                return res.status(404).json({message: "Categoria com esse ID não encontrado"})
             }
             return res.status(200).json({message: "Categoria atualizada com sucesso"})
         } else {
             return res.status(500).json(err)
+        }
+    })
+})
+
+router.delete('/delete', aut.autenticacaoToken, verRole.verificaRole, (req, res, next) => {
+    let produto = req.body
+    query = "DELETE FROM categoria WHERE id = ?"
+    connection.query(query, [produto.id], (err, results) => {
+        if(err) {
+            return res.status(500).json(err)
+        } else {
+            if(results.affectedRows == 0) {
+                return res.status(404).json({message: "Categoria com esse ID não encontrado"})
+            } else {
+                return res.status(200).json({message: "Categoria deletada com sucesso"})
+            }
         }
     })
 })
--- table.sql
+++ table.sql
@@ -13,7 +13,7 @@ CREATE TABLE user (
 // Inserindo registros na tabela "user"
 INSERT INTO user (nome, numero_contato, email, senha, status, role) VALUES ('Admin', 123456789, 'admin123@gmail.com', 'admin', 'true', 'admin');
 
-// Criando tabela "user"
+// Criando tabela "categoria"
 CREATE TABLE categoria(
     id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(250) NOT NULL,
