aa620be309536d6fcb336f4f8f5bca11372f9890
lucas.taborda <lucas.taborda@fertipar.com.br>
2023-04-12 11:26:11 -0300

    Edited jest.config.js to communicate with db

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+const { jestMysqlEnv } = require('jest-mysql');
+
+module.exports = async () => {
+
+  // Configurar as credenciais de acesso ao banco de dados
+  await jestMysqlEnv({
+    dbName: process.env.DB_NAME,
+    dbUser: process.env.DB_USERNAME,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT
+  });
+
+  return {
+    // Configurações do Jest
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   };
-  
+};
